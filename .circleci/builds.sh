1c821f1145b073136cc60c2267b6646c5781ac53
retroheim <60027238+retroheim@users.noreply.github.com>
2020-03-06 21:35:41 -0500

    Update builds.sh

--- .circleci/builds.sh
+++ .circleci/builds.sh
@@ -4,8 +4,9 @@
 set -e
 basedir="$(pwd -P)"
 #\cp -rf "$basedir/**/target/*.jar" "$basedir/builds/"
-\cp -rf "$basedir/bukkit/*/*.jar" "$basedir/builds/"
-\cp -rf "$basedir/bungee/*/*.jar" "$basedir/builds/"
-\cp -rf "$basedir/common/*/*.jar" "$basedir/builds/"
-\cp -rf "$basedir/velocity/*/*.jar" "$basedir/builds/"
+dir "$basedir/bukkit/"
+\cp -rf "$basedir/bukkit/*/*/*.jar" "$basedir/builds/"
+\cp -rf "$basedir/bungee/*/*/*.jar" "$basedir/builds/"
+\cp -rf "$basedir/common/*/*/*.jar" "$basedir/builds/"
+\cp -rf "$basedir/velocity/*/*/*.jar" "$basedir/builds/"
 )
